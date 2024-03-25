35cdf4b81520529fe1f18abedcad26fad44636f3
Leo Fablo Silalahi <leofablo@Leos-MacBook-Pro.local>
2024-03-25 13:47:37 +0700

    Exercise 1 dan 2

--- src/main/java/com/adepuu/exercises/session1/exercise1.java
+++ src/main/java/com/adepuu/exercises/session1/exercise1.java
@@ -3,17 +3,30 @@ package com.adepuu.exercises.session1;
 public class exercise1 {
 
 //    Mencari nilai tertinggi dari element array
+//    public static void main(String[] args){
+//       int[] intArrays = {1, 2, 3, 4, 5, 6, 7,};
+//        int nilaiMax = intArrays[0];
+//
+//        for(int value : intArrays){
+//            if(value > nilaiMax){
+//                nilaiMax = value;
+//            }
+//        }
+//
+//        System.out.println(nilaiMax);
+//        }
+
+
+
+        // Mencari nilai tertinggi menggunakan method
+    int[] listArray = { 1, 2, 3, 4, 5, 6, 7};
+    int maxNumber = listArray[0];
+
+    static void largerstNumber(){
+
+    }
     public static void main(String[] args){
-       int[] intArrays = {1, 2, 3, 4, 5, 6, 7,};
-        int nilaiMax = intArrays[0];
 
-        for(int value : intArrays){
-            if(value > nilaiMax){
-                nilaiMax = value;
-            }
-        }
-
-        System.out.println(nilaiMax);
-        }
+    }
     }
 
--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+
 public class Exercise2 {
     /**
      * Java Array Program For Array Rotation
@@ -9,6 +11,30 @@ public class Exercise2 {
      * So, Final result: 3, 4, 5, 6, 7, 1, 2
      */
     public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+        arrayList.add(7);
+
+        int d = 2;
+
+        for(int i = 0; i < d; i++ ){
+            int temp = arrayList.get(i);
+            arrayList.add(temp);
+        }
+
+        for(int i = 0; i < d; i++){
+            arrayList.remove(0);
+        }
+
+        System.out.println(arrayList.toString());
+
+
+
 
     }
 }
